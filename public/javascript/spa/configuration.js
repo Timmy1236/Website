18a642ce2e1e813058542258367b5931298267da
Timmy <nachodortta@hotmail.com>
2026-03-26 16:44:35 -0300

    feat: configuration usando un panel con tabs

--- public/javascript/spa/configuration.js
+++ public/javascript/spa/configuration.js
@@ -1,7 +1,7 @@
 import m from "mithril";
 import { confirmSettings, restartSettings } from "../pages/settings.js";
 import { changeLanguage } from "../core/i18n.js"
-import ButtonsRow from "../components/buttons_row.js";
+import TabPanel from "../components/tab-panel.js";
 
 const ConfigurationPage = {
   oncreate() {
@@ -9,107 +9,99 @@ const ConfigurationPage = {
     window.loadSettings?.();
   },
 
+  onupdate() {
+    window.refreshI18n?.();
+    window.loadSettings?.();
+  },
+
   view() {
     return m(".content-column", [
 
-      // SETTINGS
-      m(".panel", [
-        m(".panel-header",
-          m("h1.text-title", { "data-i18n": "settings.title" })
-        ),
+      m(TabPanel, {
+        title: { "data-i18n": "settings.title" },
+        tabs: [
+          {
+            label: "Visual",
+            content: () => m(".settings-group", [
+              m("h2.group-title", { "data-i18n": "settings.sections.visual-effects" }),
 
-        m(".panel-content", [
+              m(".option", [
+                m("input", { type: "checkbox", id: "rain-effect" }),
+                m("label", { for: "rain-effect", "data-i18n": "settings.options.rainEffects" })
+              ]),
 
-          // VISUAL EFFECTS
-          m(".settings-group", [
-            m("h2.group-title", { "data-i18n": "settings.sections.visual-effects" }),
+              m(".option", [
+                m("input", { type: "checkbox", id: "static-effect" }),
+                m("label", { for: "static-effect", "data-i18n": "settings.options.staticEffects" })
+              ]),
 
-            m(".option", [
-              m("input", { type: "checkbox", id: "rain-effect" }),
-              m("label", { for: "rain-effect", "data-i18n": "settings.options.rainEffects" })
+              m(".option", [
+                m("input", { type: "checkbox", id: "vignette-effect" }),
+                m("label", { for: "vignette-effect", "data-i18n": "settings.options.vignetteEffects" })
+              ]),
+
+              m(".settings-group", [
+                m("h2.group-title", { "data-i18n": "settings.sections.themes" }),
+
+                m(".option", [
+                  m("select#theme-select", [
+                    m("option", { value: "default" }, "[DETAILS] - Green (BROKEN)"),
+                    m("option", { value: "simple-purple" }, "[FLAT] - Purple (DEFAULT)"),
+                    m("option", { value: "simple-dark" }, "[FLAT] - Dark"),
+                    m("option", { value: "simple-red" }, "[FLAT] - Red"),
+                    m("option", { value: "simple-green" }, "[FLAT] - Green"),
+                    m("option", { value: "simple-blue" }, "[FLAT] - Blue"),
+                  ])
+                ])
+              ]),
             ]),
+          },
+          {
+            label: "Audio",
+            content: () => m(".settings-group", [
+              m("h2.group-title", { "data-i18n": "settings.sections.audio" }),
 
-            m(".option", [
-              m("input", { type: "checkbox", id: "static-effect" }),
-              m("label", { for: "static-effect", "data-i18n": "settings.options.staticEffects" })
-            ]),
+              m(".option", [
+                m("input", { type: "checkbox", id: "background-music-toggle" }),
+                m("label", { for: "background-music-toggle", "data-i18n": "settings.options.backgroundMusic" })
+              ]),
 
-            m(".option", [
-              m("input", { type: "checkbox", id: "vignette-effect" }),
-              m("label", { for: "vignette-effect", "data-i18n": "settings.options.vignetteEffects" })
-            ])
-          ]),
-
-          m(".spacing-line"),
-
-          // AUDIO
-          m(".settings-group", [
-            m("h2.group-title", { "data-i18n": "settings.sections.audio" }),
-
-            m(".option", [
-              m("input", { type: "checkbox", id: "background-music-toggle" }),
-              m("label", { for: "background-music-toggle", "data-i18n": "settings.options.backgroundMusic" })
-            ]),
-
-            m(".option", [
-              m("input", { type: "checkbox", id: "mouse-click-toggle" }),
-              m("label", { for: "mouse-click-toggle", "data-i18n": "settings.options.mouseClicks" })
-            ])
-          ]),
-
-          m(".spacing-line"),
-
-          // OTHERS
-          m(".settings-group", [
-            m("h2.group-title", { "data-i18n": "settings.sections.others" }),
-
-            m(".option", [
-              m("input", { type: "checkbox", id: "readable-font" }),
-              m("label", { for: "readable-font", "data-i18n": "settings.options.readableFont" })
-            ])
-          ]),
-
-          m(".spacing-line"),
-
-          // THEME
-          m(".settings-group", [
-            m("h2.group-title", { "data-i18n": "settings.sections.themes" }),
-
-            m(".option", [
-              m("select#theme-select", [
-                m("option", { value: "snew" }, "[DETAILS] - it snew."),
-                m("option", { value: "default" }, "[DETAILS] - Green"),
-                m("option", { value: "simple" }, "[FLAT] - Dark")
+              m(".option", [
+                m("input", { type: "checkbox", id: "mouse-click-toggle" }),
+                m("label", { for: "mouse-click-toggle", "data-i18n": "settings.options.mouseClicks" })
               ])
-            ])
-          ]),
+            ]),
+          },
+          {
+            label: "Accesibilidad",
+            content: () => m(".settings-group", [
+              m("h2.group-title", { "data-i18n": "settings.sections.others" }),
 
-          m(".spacing-line"),
-
-          // LENGUAJES
-          m(".settings-group", [
-            m("h2.group-title", { "data-i18n": "settings.sections.languages" }),
-
-            m(".option", [
-              m(".buttons-list", [
-                m("button.button", { onclick: () => changeLanguage("es"), }, "Español"),
-                m("button.button", { onclick: () => changeLanguage("en"), }, "English")
+              m(".option", [
+                m("input", { type: "checkbox", id: "readable-font" }),
+                m("label", { for: "readable-font", "data-i18n": "settings.options.readableFont" })
               ])
-            ])
-          ]),
+            ]),
+          },
+          {
+            label: "Lenguajes",
+            content: () => m(".settings-group", [
+              m("h2.group-title", { "data-i18n": "settings.sections.languages" }),
 
-          m(".spacing-line"),
-
-          // CONFIRMS
-          m(".buttons-list", [
-            m("button.button", { onclick: () => confirmSettings() }, "Confirm"),
-            m("button.button", { onclick: () => restartSettings() }, "Default Config")
-          ])
+              m(".option", [
+                m(".buttons-list", [
+                  m("button.button", { onclick: () => changeLanguage("es"), }, "Español"),
+                  m("button.button", { onclick: () => changeLanguage("en"), }, "English")
+                ])
+              ])
+            ]),
+          }
+        ],
+        outTab: m(".configuration-buttons", [
+          m("button.button", { onclick: () => confirmSettings() }, "Confirmar"),
+          m("button.button", { onclick: () => restartSettings() }, "Default Config")
         ])
-      ]),
-
-      m(ButtonsRow)
-
+      }),
     ]);
   }
 };
