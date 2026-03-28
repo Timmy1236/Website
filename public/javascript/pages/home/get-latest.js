c226f631e7a34160b38707d9307a0aeec378b967
Timmy <nachodortta@hotmail.com>
2026-03-28 02:47:41 -0300

    feat: obtener ultimo commit

--- public/javascript/pages/home/get-latest.js
+++ public/javascript/pages/home/get-latest.js
@@ -3,6 +3,15 @@ export async function getLatest() {
     const response = await fetch("/content/api/latest.json");
     return await response.json();
   } catch (e) {
-    console.error("Error loading latest posts", e);
+    console.error("get-latest.js> Error al intentar obtener los últimos blogs y changelogs: ", e);
+  }
+}
+
+export async function getLastCommit() {
+  try {
+    const response = await fetch('https://api.github.com/repos/Timmy1236/Website/commits?per_page=1')
+    return await response.json();
+  } catch (e) {
+    console.error("get-latest.js> Error al intentar obtener los últimos commits: ", e);
   }
 }
