f6264e6f77a647d269827c9aad85e97a725e9c4b
Timmy <nachodortta@hotmail.com>
2026-03-30 05:04:27 -0300

    feat: texto wave jejeks :)

--- public/javascript/components/banner.js
+++ public/javascript/components/banner.js
@@ -1,11 +1,21 @@
 import m from "mithril";
 
+const text = "timmy.nekoweb.com";
+
 export default {
   view: () =>
     m(".banner-panel",
       m(".banner", [
         m("img"),
-        m("p.banner-title", "timmy.nekoweb.org")
+        m("p.banner-title",
+          m(".wave-text",
+            text.split("").map((char, i) =>
+              m("span", {
+                style: { "--i": i }
+              }, char)
+            )
+          )
+        )
       ])
     )
 };
