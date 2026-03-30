9e6d4d5dac98635d1f2b3979bf5fb4da81d6a552
Timmy <nachodortta@hotmail.com>
2026-03-30 04:55:11 -0300

    feat: funcionalidades en los botones de los paneles

---
+++ public/javascript/ui/panel-buttons.js
@@ -0,0 +1,44 @@
+/* 
+ * panel-buttons.js
+ * ----------------
+ * Encargado de manejar la lógica de los botones de los paneles, minimizar o cerrar paneles.
+ * También encargado en automáticamente mimetizar ciertos paneles que tengan: { "data-default": "closed" }
+*/
+
+document.addEventListener("click", (e) => {
+  const btn = e.target.closest("[data-panel-action]");
+  if (!btn) return;
+
+  const panel = btn.closest(".panel");
+  if (!panel) return;
+
+  const action = btn.dataset.panelAction;
+
+  if (action === "minimize") {
+    const content = panel.querySelector(".panel-content");
+    if (!content) return;
+
+    content.classList.toggle("collapsed");
+
+    const isCollapsed = content.classList.contains("collapsed");
+    btn.textContent = isCollapsed ? "▲" : "▼";
+  }
+
+  if (action === "close") {
+    panel.remove();
+  }
+});
+
+/**
+ * Añade funcionalidades a los botones de los paneles.
+ */
+export function initPanel() {
+  document.querySelectorAll(".panel[data-default='closed']").forEach(panel => {
+    console.log(panel)
+    const content = panel.querySelector(".panel-content");
+    const btn = panel.querySelector("[data-panel-action='minimize']");
+
+    if (content) content.classList.add("collapsed");
+    if (btn) btn.textContent = "▲";
+  });
+}
