0d77d4d0eb821ac794cdc00d456f69118d1411a2
Timmy <nachodortta@hotmail.com>
2026-04-06 05:25:42 -0300

    fix: css que entraban en el scan de stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
   "extends": ["stylelint-config-standard"],
-  "ignoreFiles": ["./public/pages/archive/2025/*.css", "./public/elements.css"]
+  "ignoreFiles": ["./public/pages/archive/2025/*.css",
+    "./public/pages/others/tile/*.css",
+    "./public/elements.css",
+    "./public/dist/*.css"]
 };
