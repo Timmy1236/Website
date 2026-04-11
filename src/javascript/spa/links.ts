45c4d18d6e32e0868537bf2ff00071b671399c88
Timmy <nachodortta@hotmail.com>
2026-04-11 04:37:01 -0300

    feat: links actualizados, nuevos creditos, nuevo vecinos

--- src/javascript/spa/links.ts
+++ src/javascript/spa/links.ts
@@ -11,7 +11,7 @@ const Others = {
 
       m(".panel",
         m(".panel-header", [
-          m("h1", { "data-i18n": "neighbors.title" }),
+          m("p", { "data-i18n": "neighbors.title" }),
 
           m(".panel-controls", [
             m("button.panel-button", { "data-panel-action": "minimize" }, "▼"),
@@ -21,25 +21,34 @@ const Others = {
         m(".panel-content", [
           m(".neighbors", [
             m("a", { href: "https://chantu.nekoweb.org/", "data-tooltip": "Chantu Website!" },
-              m("img", { src: "https://chantu.nekoweb.org/imgs/buttons/Chantu.png", alt: "Chantu website" })
+              m("img", { src: "https://chantu.nekoweb.org/imgs/buttons/Chantu.png", alt: "Chantu button" })
             ),
             m("a", { href: "https://slowslushie.nekoweb.org/", "data-tooltip": "Slowslushie Website!" },
-              m("img", { src: "/assets/images/buttons/slowslushie-fix.png", alt: "slowslushie website" })
+              m("img", { src: "/assets/images/buttons/slowslushie-fix.png", alt: "slowslushie button" })
             ),
             m("a", { href: "https://nyani58.nekoweb.org/", "data-tooltip": "NYANI58 Website!" },
-              m("img", { src: "https://file.garden/Z6nZpUcjoTvBXgF3/assets/realimportant/nyani58_button1.gif", alt: "nyani58 website" })
+              m("img", { src: "https://file.garden/Z6nZpUcjoTvBXgF3/assets/realimportant/nyani58_button1.gif", alt: "nyani58 button" })
             ),
             m("a", { href: "https://nogood-angel.nekoweb.org/home", "data-tooltip": "No-Good Angel Website!" },
-              m("img", { src: "https://nogood-angel.neocities.org/images/graphics/button_nogood-angel.png", alt: "nogood-angel website" })
+              m("img", { src: "https://nogood-angel.neocities.org/images/graphics/button_nogood-angel.png", alt: "nogood-angel button" })
             ),
             m("a", { href: "https://someones-insane.nekoweb.org/", "data-tooltip": "Someones-Insane Website!" },
-              m("img", { src: "https://someones-insane.nekoweb.org/images/imapettyasshole_button.gif", alt: "Someones-Insane Website!" })
+              m("img", { src: "https://someones-insane.nekoweb.org/images/imapettyasshole_button.gif", alt: "Someones-Insane button" })
+            ),
+            m("a", { href: "https://yefreitor.com", "data-tooltip": "yefreitor Website!" },
+              m("img", { src: "https://yefreitor.com/img/button.png", alt: "Someones-Insane button" })
             ),
             m("a", { href: "https://xyzko.nekoweb.org/", "data-tooltip": "xyzko Website!" },
-              m("img", { src: "/assets/images/buttons/no-button.png", alt: "xyzko website" })
+              m("img", { src: "/assets/images/buttons/no-button.png", alt: "placeholder button" })
             ),
             m("a", { href: "https://bweh.dhyotteh.com/", "data-tooltip": "¡Bwéh! Website!" },
-              m("img", { src: "/assets/images/buttons/no-button.png", alt: "¡Bwéh! Website!" })
+              m("img", { src: "/assets/images/buttons/no-button.png", alt: "placeholder button" })
+            ),
+            m("a", { href: "https://jovidmtp.nekoweb.org", "data-tooltip": "(jovidmtp) This is a website!" },
+              m("img", { src: "/assets/images/buttons/no-button.png", alt: "placeholder button" })
+            ),
+            m("a", { href: "https://ramon.nekoweb.org/", "data-tooltip": "Ramón Website!" },
+              m("img", { src: "/assets/images/buttons/no-button.png", alt: "placeholder button" })
             ),
           ])
         ])
@@ -48,7 +57,7 @@ const Others = {
       // BUTTON
       m(".panel", [
         m(".panel-header",
-          m("h1.text-title", "Button"),
+          m("p.text-title", "Button"),
 
           m(".panel-controls", [
             m("button.panel-button", { "data-panel-action": "minimize" }, "▼"),
@@ -81,7 +90,7 @@ https://file.garden/aSqYsBZqpx5ZY3su/media/img/nekoweb/timmy_button.png
       // Créditos
       m(".panel", [
         m(".panel-header",
-          m("h1.text-title", { "data-i18n": "links.credits.title" }),
+          m("p.text-title", { "data-i18n": "links.credits.title" }),
 
           m(".panel-controls", [
             m("button.panel-button", { "data-panel-action": "minimize" }, "▼"),
@@ -90,7 +99,7 @@ https://file.garden/aSqYsBZqpx5ZY3su/media/img/nekoweb/timmy_button.png
         ),
         m(".panel-content", [
 
-          m("h3", "Hosting:"),
+          m("p.heading", "[ Hosting ]"),
           m("ul", [
             m("li", [m("a.link", { href: "https://nekoweb.org/", target: "_blank" }, "Website: Nekoweb")]),
             m("li", [m("a.link", { href: "https://filegarden.com/", target: "_blank" }, "Images: File Garden")]),
@@ -98,32 +107,32 @@ https://file.garden/aSqYsBZqpx5ZY3su/media/img/nekoweb/timmy_button.png
 
           m(".spacing-line", { style: "--spacing-margin: 10px;" }),
 
-          m("h3", "Softwares:"),
+          m("p.heading", "[ Librerías ]"),
           m("ul", [
             m("li", [m("a.link", { href: "https://mithril.js.org/", target: "_blank" }, "Mithril: Single Page Application")]),
             m("li", [m("a.link", { href: "https://www.11ty.dev/", target: "_blank" }, "11ty: Static Site Generator")]),
-            m("li", [m("a.link", { href: "https://swup.js.org/", target: "_blank" }, "Swup: Page Transition")]),
-            m("li", [m("a.link", { href: "https://esbuild.github.io/", target: "_blank" }, "esbuild: Bundler")]),
+            m("li", [m("a.link", { href: "https://esbuild.github.io/", target: "_blank" }, "esbuild: TypeScript & CSS Bundler")]),
           ]),
 
           m(".spacing-line", { style: "--spacing-margin: 10px;" }),
 
-          m("h3", "Herramientas externas:"),
+          m("p.heading", "[ Herramientas Externas ]"),
           m("ul", [
             m("li", [m("a.link", { href: "https://ditherit.com/", target: "_blank" }, "Dither it!")]),
             m("li", [m("a.link", { href: "https://compress-or-die.com/", target: "_blank" }, "Compress or Die")]),
+            m("li", [m("a.link", { href: "https://ezgif.com/", target: "_blank" }, "Ezgif")]),
           ]),
 
           m(".spacing-line", { style: "--spacing-margin: 10px;" }),
 
-          m("h3", "Audios/Canciones"),
+          m("p.heading", "[ Audios & Canciones ]"),
           m("ul", [
-            m("li", [m("a.link", { href: "https://www.youtube.com/watch?v=M-DAQh5HPjo", target: "_blank" }, `"Grace FM"`)])
+            m("li", [m("a.link", { href: "https://germfood.bandcamp.com/album/night-of-the-consumers-ost", target: "_blank" }, `"Store Track 1: Main website background song"`)])
           ]),
 
           m(".spacing-line", { style: "--spacing-margin: 10px;" }),
 
-          m("h3", "Imágenes"),
+          m("p.heading", "[ Imágenes ]"),
           m("ul", [
             m("li", [m("a.link", { href: "https://www.reddit.com/r/LiminalSpace/comments/19ek506/voices_of_the_void/", target: "_blank" }, "Banner & others VotV images")]),
           ]),
@@ -133,7 +142,7 @@ https://file.garden/aSqYsBZqpx5ZY3su/media/img/nekoweb/timmy_button.png
       // WebTiles
       m(".panel", [
         m(".panel-header",
-          m("h1.text-title", "iFrame: WebTiles"),
+          m("p.text-title", "iFrame: WebTiles"),
 
           m(".panel-controls", [
             m("button.panel-button", { "data-panel-action": "minimize" }, "▼"),
@@ -159,7 +168,7 @@ https://timmy.nekoweb.org/pages/others/tile/`
 
       m(".panel", [
         m(".panel-header",
-          m("h1.text-title", "TV TIME (SUPER W.I.P)"),
+          m("p.text-title", "TV TIME (SUPER W.I.P)"),
 
           m(".panel-controls", [
             m("button.panel-button", { "data-panel-action": "minimize" }, "▼"),
