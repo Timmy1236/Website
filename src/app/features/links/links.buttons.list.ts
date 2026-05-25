2c4be641f11920e5ba981490165ab54b53c69705
Timmy <nachodortta@hotmail.com>
2026-05-25 16:08:43 -0300

    feat: separando los buttons de la logica de la pagina

---
+++ src/app/features/links/links.buttons.list.ts
@@ -0,0 +1,104 @@
+import { type ButtonSite } from "./links.buttons.overlay.ts";
+
+export const neighborSites: ButtonSite[] = [
+  {
+    href: "https://chantu.nekoweb.org/",
+    btnSrc: "https://chantu.nekoweb.org/imgs/buttons/Chantu.png",
+    screenshotSrc: "/assets/images/pages/links/previews/chantu.webp",
+    text: "La primera persona que conocí en nekoweb y de paso que hablase en español, si no fuera por Chantu seguramente me hubiera quedado en github pages por lo complicado que se ve todo esto sitio. ¡Super friendly!",
+  },
+  {
+    href: "https://slowslushie.nekoweb.org/",
+    btnSrc: "/assets/images/buttons/slowslushie-fix.png",
+    screenshotSrc: "/assets/images/pages/links/previews/slowslushie.webp",
+    text: "Banner por encima con una anchura que cubre el sidebar y el contenido principal, el sidebar a la izquierda con botones para cambiar de pagina... El contenido principal a la derecha... ¡Mi pagina es original y no se lo robe a nadie!",
+  },
+  {
+    href: "https://nyani58.nekoweb.org/",
+    btnSrc: "https://file.garden/Z6nZpUcjoTvBXgF3/assets/realimportant/nyani58_button1.gif",
+    screenshotSrc: "/assets/images/pages/links/previews/nyani58.webp",
+    text: "¿¿¿DONDE MIERDA ESTOY???",
+  },
+  {
+    href: "https://nogood-angel.nekoweb.org/",
+    btnSrc: "https://nogood-angel.moe/images/graphics/buttons/button2.webp",
+    screenshotSrc: "/assets/images/pages/links/previews/nogoodangel.webp",
+    text: "Cada vez que entro a nekoweb, veo esta misma pagina siempre estando primero en la categoría 'ultimas updates'. ¿Como? ¿Como alguien puede sacar tantas updates? ¿Como alguien le da la cabeza para eso?",
+  },
+  {
+    href: "https://someones-insane.nekoweb.org/",
+    btnSrc: "https://someones-insane.nekoweb.org/images/imapettyasshole_button.gif",
+    screenshotSrc: "/assets/images/pages/links/previews/someonesInsane.webp",
+    text: "No se porque... Pero la imagen del inicio me hace recordar demasiado a los juegos de terror hecho en flash de 'Point & Click' donde los objetos clickeables se verían estúpidamente diferente a los demás. Me hubiera gustado que fuera un flash de verdad corriendo en Ruffle"
+  },
+  {
+    href: "https://yefreitor.com/",
+    btnSrc: "https://yefreitor.com/img/button.png",
+    screenshotSrc: "/assets/images/pages/links/previews/yefreitor.webp",
+    text: "Hay un maldito botón para fumarte un porro en el inicio. ¿Que mas necesitas?"
+  },
+  {
+    href: "https://jovidmtp.nekoweb.org/",
+    btnSrc: "/assets/images/buttons/no-button.png",
+    screenshotSrc: "/assets/images/pages/links/previews/jovidmtp.webp",
+    text: "Pobre alma, su pagina siendo super compatible con versiones super antiguas para navegadores! ¡Y la mia necesitaras la ultima version posible de Chrome y ni con eso seras capaz de cargarlo sin problemas!"
+  },
+  {
+    href: "https://cbz.nekoweb.org/",
+    btnSrc: "/assets/images/buttons/no-button.png",
+    screenshotSrc: "/assets/images/pages/links/previews/cbz.webp",
+    text: "Triste de ver que YouPlock cerrara pronto, era silly pero tenia potencial teniendo en cuneta que se veía algo muy complicado de armar. Espero lo mejor para siguientes proyectos.",
+  },
+  {
+    href: "https://ellen.nekoweb.org/",
+    btnSrc: "https://ellen.nekoweb.org/Images/oc%20headshot%20style.png",
+    screenshotSrc: "/assets/images/pages/links/previews/ellen.webp",
+    text: "¿Una lista de badges difíciles de conseguir? Me gusta esa idea. Debería de poner mas cosas de Roblox en mi sitio.",
+  },
+  {
+    href: "https://ramon.nekoweb.org/",
+    btnSrc: "https://ramon.nekoweb.org/botonesyblinkies/botonramon.png",
+    screenshotSrc: "/assets/images/pages/links/previews/ramon.webp",
+    text: "No sabes lo difícil que fue encontrar otra pagina en nekoweb que el autor hable español, 'RaMóN' NO TE ME DESAPAREZCAS, NECESITO CONOCER MAS GENTE QUE HABLE EN ESPAÑOL EN NEKOWEB.",
+  },
+  {
+    href: "https://amoeba.nekoweb.org/",
+    btnSrc: "/assets/images/buttons/no-button.png",
+    screenshotSrc: "/assets/images/pages/links/previews/amoeba.webp",
+    text: "No, la captura no esta mal. La pagina cargo correctamente. Así es el landing page. Me quede como un minuto mirando la pantalla esperando que algo pasara, y tenia que clickear el titulo para continuar. :)"
+  },
+  {
+    href: "https://reduxflakes.nekoweb.org/",
+    btnSrc: "https://reduxflakes.nekoweb.org/img/hcl6TIROJn-88.webp",
+    screenshotSrc: "/assets/images/pages/links/previews/reduxflakes.webp",
+    text: null
+  },
+  {
+    href: "https://lucksiyo.xyz/",
+    btnSrc: "https://lucksiyo.xyz/resources/button_v1.jpg",
+    screenshotSrc: "/assets/images/pages/links/previews/lucksiyo.webp",
+    text: "Esto tiene una mejor sensación de fluidez comparado a lo que yo mismo estoy haciendo"
+  },
+];
+
+export const likesSite: ButtonSite[] = [
+  {
+    href: "https://jbc.lol/",
+    btnSrc: "https://jbc.lol/imgs/buttons/jbtn.png",
+    screenshotSrc: "/assets/images/pages/links/previews/jbc.webp",
+    text: "Nekobox realmente me ayudo demasiado para tener un sitebox al menos decente, me parece extraño que eso no sea una función nativa en nekoweb, tengo problemas con el css de mi propia pagina y estos quieren que lo haga a ciegas???",
+  },
+  {
+    href: "https://moosyu.nekoweb.org/",
+    btnSrc: "https://moosyu.github.io/assets/buttons/moosyu.png",
+    screenshotSrc: "/assets/images/pages/links/previews/moosyu.webp",
+    text: "Me robe el código del form de esta misma pagina y el owner no lo sabe :youtube_emoji_biting_nails:",
+  },
+];
+
+export const MyButton: ButtonSite = {
+  href: "https://timmy.nekoweb.org/",
+  btnSrc: "https://timmy.nekoweb.org/assets/images/buttons/timmy_button.png",
+  screenshotSrc: "/assets/images/pages/links/previews/timmy.webp",
+  text: "... Ese soy yo.",
+}
