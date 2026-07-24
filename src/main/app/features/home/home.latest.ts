7475b2bb4c1e0ad271a9f0b300eb16d060305b04
Timmy <nachodortta@hotmail.com>
2026-07-24 00:30:52 -0300

    refactor: cambiando "api" a "latest"

--- src/main/app/features/home/home.latest.ts
+++ src/main/app/features/home/home.latest.ts
@@ -2,7 +2,7 @@ import { getSetting } from "../../shared/core/settings-logic";
 
 export async function getLatest() {
 	const local = getSetting("preferred-language") || "en";
-	const url = local === "es" ? "/content/api/latest_es.json" : "/content/api/latest_en.json";
+	const url = local === "es" ? "/content/latest/es.json" : "/content/latest/en.json";
 
 	const response = await fetch(url);
 	if (!response.ok) throw new Error(`HTTP ${response.status}`);
